1740d0901eaf2506945d0fbbda891db726190730
hkchaudhari-25 <hkc2k222@gmail.com>
2026-08-13 09:50:13 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req, res) => {
+    res.send('we are at twitter route....')
+})
+
+app.get('/chaha', (req, res)=>{
+    res.send('Take chaha.. even in Unhala..')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
