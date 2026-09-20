ef03bb56dfdaa5f70f957823e32e8c7c99f068da
hkchaudhari-25 <hkc2k222@gmail.com>
2026-09-20 16:47:22 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import app from "./app.js"
 
 dotenv.config({
     path: './.env'
 });
 
 console.log("MONGODB_URL:", process.env.MONGODB_URL);
+app.on("error", (error) => {
+    console.log("App error:", error);
+    throw error;
+});
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 800 , () => {
+        console.log(`Server is running on PORT :  ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log(`MONGODB connection failed !!! `, error)
+})
 
 /*
 import express from "express"
 const app= express();
-
 ( async () => {
     try {
 mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,     //http status code
+        message = "Something went wrong.." , //do not prefer this message in production level code
+        errors= [] , //addition error datils
+        statck = "" //stack trace
+    ){  
+        super(message)      //parent Error constuctor
+        this.statusCode = statusCode //http status code
+        this.data = null                //Since this is an error response, there usually isn't useful data to return
+        this.success = false
+        this.message= message           
+        this.errors=errors
+
+        //A stack trace tells you where the error happened This is extremely useful for debugging
+        if(statck){
+            this.stack= statck  //stack trace
+        } else {
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError}
